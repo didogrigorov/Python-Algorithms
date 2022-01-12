f85675760ea12d48cfda4050d5324c253436b3b2
Dilyan Grigorov <dido.grigorov@gmail.com>
2022-01-12 15:05:07 +0200

    Update combine-list-value-2-by-2.py

--- combine-list-value-2-by-2.py
+++ combine-list-value-2-by-2.py
@@ -1,9 +1,13 @@
-arr = [1,2,3,4,5,6]
+numbers = list(input("Enter even number of digits: "))
 
+for item in range(0, len(numbers)):
+  numbers[item] = int(numbers[item])
+
+print(numbers)
 sum = 0
 count = 0
 
-for i in range(0, len(arr) // 2):
-  sum = arr[i+count] + arr[i+1+count]
+for i in range(0, len(numbers) // 2):
+  sum = numbers[i+count] + numbers[i+1+count]
   count += 1
   print(sum)
